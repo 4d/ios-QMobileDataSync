fd7ffc73c758834c1939356bcbd7740933a23989
Eric Marchand <eric.marchand@4d.com>
2018-09-11 16:28:02 +0100

    [Mobile] Less verbose if no primary key is defined in table (no warning)
    Imported from Git
     Author: Eric Marchand <eric.marchand@4d.com> 1536679675 +0200
     Committer: Eric Marchand <eric.marchand@4d.com> 1536679675 +0200
     sha1: 3034c34716dbd5aaa1eef832d2790ee69f837afd
     push-state: complete
     parent-changes: 49df2f4bc15d352d2f9f95c16dcc2e636216483f=[224616]
    
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileDataSync/": change = 227225]

--- Sources/DataSync+Records.swift
+++ Sources/DataSync+Records.swift
@@ -32,7 +32,7 @@ extension DataSync {
                     if let predicate = table.predicate(for: json) {
                         record = try context.getOrCreate(in: tableInfo.name, matching: predicate)
                     } else {
-                        logger.warning("Cannot checking if record already in database, no primary key for table '\(tableName)'. This could result to duplicate records.")
+                        logger.debug("Cannot checking if record already in database, no primary key for table '\(tableName)'. This could result to duplicate records.")
                         record = context.create(in: tableInfo.name)
                         // assertionFailure("Table \(tableInfo.name) must have primary key")
                     }
