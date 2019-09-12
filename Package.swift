9172d896b6efe84e7aeb0dd0c70667accf705507
quentin marciset <quentin.marciset@4d.com>
2019-09-12 10:36:08 +0100

    [Mobile] updating Package.swift file, DeviceKit not compatible with UIKit #107928 - WIP, need to test more dependencies
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileDataSync/": change = 241437]

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/phimage/Prephirences.git", .revision("HEAD")),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0")
+        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0")
     ],
     targets: [
         .target(
@@ -38,7 +38,7 @@ let package = Package(
                 "Moya",
                 "SwiftyJSON",
                 "Prephirences",
-                "DeviceKit"
+                // "DeviceKit"
             ],
             path: "Sources"),
         .testTarget(
