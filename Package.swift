26bdd526126e96efc4639867fc3b2c8a105661a2
Eric Marchand <eric.marchand@4d.com>
2019-11-07 09:36:24 +0100

    [Mobile] Update packages: Moya/Alamofire. Set swift 5.1 Fix QMobileUI workspace
    Jobs: redmine:108873
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileDataSync/": change = 244190]

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "QMobileDataSync",
     platforms: [
         .macOS(.v10_14),
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "QMobileDataSync", targets: ["QMobileDataSync"]),
@@ -19,11 +19,11 @@ let package = Package(
         .package(url: "https://github.com/Thomvis/BrightFutures.git", from: "8.0.1"),
         .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
-        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.3"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.5"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
-        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0")
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0")
     ],
     targets: [
         .target(
