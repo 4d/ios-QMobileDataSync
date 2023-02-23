2ea42d489730248f3ac4c2d0da98fc1c17cbb544
eric.marchand <eric.marchand@4d.com>
2023-02-23 08:06:07 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,14 +17,14 @@ let package = Package(
         .package(url: "https://gitlab-4d.private.4d.fr/4d/qmobile/ios/QMobileAPI.git", .revision("HEAD")),
         .package(url: "https://gitlab-4d.private.4d.fr/4d/qmobile/ios/QMobileDataStore.git", .revision("HEAD")),
 
-        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
+        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.1.0"),
 
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.4.0"),
 
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .revision("5.0.0")),
-        .package(url: "https://github.com/Moya/Moya.git", .revision("14.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.4"),
+        .package(url: "https://github.com/Moya/Moya.git",  from:"15.0.3"),
     ],
     targets: [
         .target(
